12c08c03e045aaaf5fb4b7f8831ff9bacbf3ff65
Matamenas <B00149694@mytudublin.ie>
2023-10-25 12:50:00 +0100

    Upload #1

---
+++ Assets/Challenge 3/Scripts/OpenLink.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenLink : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public void OpenRep()
+    {
+        Application.OpenURL("https://github.com/Matamenas/Chal3_B00149694_Matas");
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
